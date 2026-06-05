257b87ebfb7887882783c242ab8a2cfa21dd0249
Mobarok90 <140237996+Mobarok90@users.noreply.github.com>
2026-06-05 17:32:02 +0600

    Change ADMIN_ID in bot.py
    Updated the ADMIN_ID for the Telegram bot.

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 
 # --- কনফিগারেশন ---
 # ⚠️ নিচে আপনার টেলিগ্রাম আইডি নম্বরটি বসিয়ে দিন (যেমন: ADMIN_ID = 12345678)
-ADMIN_ID = 123456789 
+ADMIN_ID = 5165615512 
 
 BOT_TOKEN = "8803328478:AAEpVHyLj4svKmfktuewTMZP_1ydvu9zdCQ"
 bot = telebot.TeleBot(BOT_TOKEN)
